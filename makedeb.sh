734b7bcbcb96c7947f458b3a138893f0750377a2
Michael Schwarz <michael.schwarz91@gmail.com>
2021-05-15 20:05:14 +0200

    update to version 0.3

--- makedeb.sh
+++ makedeb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 name=beamer-preview
-VERSION=0.2
+VERSION=0.3
 if test `whoami` != "root"; then echo "You need to run this target using fakeroot: fakeroot -u make deb"; exit 1; fi
 mkdir -pv deb/usr/bin
 mkdir -pv deb/usr/share/doc/$name/
@@ -9,7 +9,7 @@ cp $name.py deb/usr/bin/$name
 mkdir -p deb/DEBIAN
 sed "s/%VERSION%/$VERSION/" debian-control > deb/DEBIAN/control
 echo "initial version" > deb/usr/share/doc/$name/changelog
-echo "Copyright 2020, Michael Schwarz" > deb/usr/share/doc/$name/copyright
+echo "Copyright 2021, Michael Schwarz" > deb/usr/share/doc/$name/copyright
 gzip -c -9 -n deb/usr/share/doc/$name/changelog > deb/usr/share/doc/$name/changelog.gz
 gzip -c -9 -n $name.1 > deb/usr/share/man/man1/$name.1.gz
 rm deb/usr/share/doc/$name/changelog
